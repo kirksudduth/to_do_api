48cc2204d1091c5b2206cd6fdac9a8b5206d26ac
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-21 15:26:25 -0600

    building out serializers.py

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,2 +1,13 @@
 from rest_framework import serializers
 
+from .models import Post
+
+from django import forms
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'title', 'description',
+        )
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import PostSerializer
+from .models import Post
+
 class TestView(APIView):
     def get(request, *args, **kwargs):
         data = {
@@ -13,4 +16,10 @@ class TestView(APIView):
         }
         return Response(data)
 
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 # Create your views here.
