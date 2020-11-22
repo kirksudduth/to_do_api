305f9d35afa5496bda6bdf2047ccf9e669cae89e
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-22 15:31:52 -0600

    got token from post with username and password

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from todo.views import *
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', TestView.as_view(), name="test"),
-    path('register/', register, name="register")
+    path('register/', register, name="register"),
+    path('api/token/', obtain_auth_token, name="obtain-token")
 ]
