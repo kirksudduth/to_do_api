18be32c1826e42837cc11e670e7d251cab2ff4b9
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-22 10:25:17 -0600

    got register.py built out but it is with templates and not react so i think i need to refactor

--- todo/views/auth/register.py
+++ todo/views/auth/register.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.views.decorators.csrf import csrf_exempt
+from ...forms.register import RegisterForm
+
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            authenticated_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            login(request, authenticated_user)
+
+            return redirect("/")
+    
+    else:
+        form = RegisterForm()
+        context = {
+            'form': form
+        }
+
+        return render(request, "registration/register.html", context)
