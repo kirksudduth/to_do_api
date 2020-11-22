2379373411865e984c3ae4b61fb271b90b806263
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-22 10:40:16 -0600

    think i need to merge master because it's not up to date with most current branch on gitHub

--- todo/views/views.py
+++ todo/views/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 
 # 3rd party imports
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated,)
     def get(request, *args, **kwargs):
         data = {
             "Name": "Gary",
@@ -13,4 +16,5 @@ class TestView(APIView):
         }
         return Response(data)
 
-# Create your views here.
+    
+
--- todo_api/urls.py
+++ todo_api/urls.py
@@ -6,5 +6,6 @@ from todo.views import *
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name="test")
+    path('', TestView.as_view(), name="test"),
+    path('register/', register, name="register")
 ]
