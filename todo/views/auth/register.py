f25d856562236fa5d212948620b25307cf741a7d
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-22 10:17:44 -0600

    added some imports to register.py

---
+++ todo/views/auth/register.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from todo.views import TestView
+from todo.views import *
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
